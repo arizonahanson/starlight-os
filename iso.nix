765612b0c8b12fc3e4181d8382bdb3da35cf565f
Isaac W Hanson <isaac@starlig.ht>
2018-11-03 17:30:53 -0400

    iso.nix

---
+++ iso.nix
@@ -0,0 +1,10 @@
+{config, pkgs, ...}:
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+
+    # Provide an initial copy of the NixOS channel so that the user
+    # doesn't need to run "nix-channel --update" first.
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+}
