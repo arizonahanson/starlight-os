3821c30b8207c70ffb858b5d15a7858b629db7c6
Isaac W Hanson <isaac@starlig.ht>
2018-11-04 12:33:20 -0500

    disable adding networkmanager group for now

--- config/networking.nix
+++ config/networking.nix
@@ -7,7 +7,7 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  users.users.admin.extraGroups = [ "networkmanager" ];
+  #users.users.admin.extraGroups = [ "networkmanager" ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
