d43e89f60ce31023d7da944d3d269b92ef61cbd3
Isaac W Hanson <isaac@starlig.ht>
2018-11-03 22:14:18 -0400

    just sensible and vim-nix to start

--- config/vim.nix
+++ config/vim.nix
@@ -9,13 +9,9 @@
         # E.g. set this to "my-vim" and you need to type "my-vim" to open this vim
         # This allows to have multiple vim packages installed (e.g. with a different set of plugins)
         name = "vim";
-      	vimrcConfig.customRC = ''
-      	  " Here one can specify what usually goes into `~/.vimrc` 
-      	  syntax enable
-      	'';
       	vimrcConfig.packages.vim = with pkgs.vimPlugins; {
       	  # loaded on launch
-      	  start = [ ale fugitive vim-nix vim-sensible ];
+      	  start = [ vim-nix vim-sensible ];
       	  # manually loadable by calling `:packadd $plugin-name`
       	  #opt = [ phpCompletion elm-vim ];
       	  # To automatically load a plugin when opening a filetype, add vimrc lines like:
