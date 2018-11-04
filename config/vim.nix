b9efb547c593e0bde291ca0b5ae88c51a3ab471c
Isaac W Hanson <isaac@starlig.ht>
2018-11-03 22:38:27 -0400

    vim-sensible

--- config/vim.nix
+++ config/vim.nix
@@ -3,13 +3,13 @@
 {
   programs.vim.defaultEditor = true;
   environment.systemPackages = with pkgs; [
-    (with import <nixpkgs> {};
+    vim (with import <nixpkgs> {};
       vim_configurable.customize {
         # Specifies the vim binary name.
         # E.g. set this to "my-vim" and you need to type "my-vim" to open this vim
         # This allows to have multiple vim packages installed (e.g. with a different set of plugins)
-        name = "vim";
-      	vimrcConfig.packages.vim = with pkgs.vimPlugins; {
+        name = "vim-sensible";
+      	vimrcConfig.packages.myVimPackage = with pkgs.vimPlugins; {
       	  # loaded on launch
       	  start = [ vim-nix vim-sensible ];
       	  # manually loadable by calling `:packadd $plugin-name`
