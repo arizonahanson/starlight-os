0d710119d41a66db7d683dcbb384d8e2d82b57ea
Isaac W Hanson <isaac@starlig.ht>
2018-11-03 21:41:23 -0400

    custom vim hook

--- config/vim.nix
+++ config/vim.nix
@@ -3,7 +3,18 @@
 {
   programs.vim.defaultEditor = true;
   environment.systemPackages = with pkgs; [
-    
+		(with import <nixpkgs> {};
+
+		vim_configurable.customize {
+	    # Specifies the vim binary name.
+	    # E.g. set this to "my-vim" and you need to type "my-vim" to open this vim
+	    # This allows to have multiple vim packages installed (e.g. with a different set of plugins)
+	    name = "vim";
+	    vimrcConfig.customRC = ''
+        # Here one can specify what usually goes into `~/.vimrc` 
+        syntax enable
+  	  '';
+		})    
   ];
 }
 
