b0e0140c1e1a193b74294bc00f68ba66adc22c8c
Isaac W Hanson <isaac@starlig.ht>
2018-11-10 23:48:50 -0500

    cruft

--- config/vim.nix
+++ config/vim.nix
@@ -15,17 +15,13 @@
       vim_minimal = (with import <nixpkgs> {}; 
         vim_config.customize {
           # Specifies the vim binary name.
-          # E.g. set this to "my-vim" and you need to type "my-vim" to open this vim
-          # This allows to have multiple vim packages installed (e.g. with a different set of plugins)
           name = "vim";
           vimrcConfig = {
             packages.nix_sensible = with pkgs.vimPlugins; {
         	    # loaded on launch
               start = [ vim-sensible vim-colors-solarized ];
-              # manually loadable by calling `:packadd $plugin-name`
+              # manually loadable by calling `:packadd plugin-name`
               opt = [ vim-nix ];
-              # To automatically load a plugin when opening a filetype, add vimrc lines like:
-              # autocmd FileType html :packadd plugin-name
             };
             customRC = ''
               set background=light
