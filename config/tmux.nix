68e8b20c2af2e102decaaaa8abc6e1f85ec66546
Isaac W Hanson <ihanson@sevone.com>
2019-01-08 11:46:48 -0500

    tmux advances

--- config/tmux.nix
+++ config/tmux.nix
@@ -10,53 +10,60 @@
   environment.etc."tmux.conf" = {
     mode = "0644";
     text = ''
-set -g default-terminal "screen-256color"
-set -g set-titles on
-set -g set-titles-string "#W"
-set -g monitor-activity on
-set -g escape-time 0
-set -g status "on"
-set -g status-left-length "100"
-set -g status-right-length "100"
-set -g status-right "#[fg=colour7]#S  "
-set -g status-left ""
-setw -g window-status-separator ""
-setw -g window-status-current-format " #I #W "
-setw -g window-status-format " #I #W "
+      set -g default-terminal "screen-256color"
+      set -g set-titles on
+      set -g set-titles-string "#W"
+      set -g monitor-activity on
+      set -g escape-time 0
+      set -g status "on"
+      set -g status-left-length "100"
+      set -g status-right-length "100"
+      set -g status-right "#[fg=colour7]#S  "
+      set -g status-left ""
+      set -g renumber-windows on
+      setw -g window-status-separator ""
+      setw -g window-status-current-format " #W "
+      setw -g window-status-format " #W "
+      set -g base-index 1
+      setw -g pane-base-index 1
 
-#### COLOUR (Solarized dark)
+      # default statusbar colors
+      set-option -g status-bg default
+      set-option -g status-fg yellow
+      set-option -g status-attr default
 
-# default statusbar colors
-#set-option -g status-bg black #base02
-set-option -g status-bg default
-set-option -g status-fg yellow #yellow
-set-option -g status-attr default
+      # default window title colors
+      set-window-option -g window-status-fg brightblack
+      set-window-option -g window-status-bg default
+      #set-window-option -g window-status-attr dim
 
-# default window title colors
-set-window-option -g window-status-fg brightblack #base0
-set-window-option -g window-status-bg default
-#set-window-option -g window-status-attr dim
+      # active window title colors
+      set-window-option -g window-status-current-fg brightwhite
+      set-window-option -g window-status-current-bg brightblack
+      #set-window-option -g window-status-current-attr bright
 
-# active window title colors
-#set-window-option -g window-status-current-fg brightred #orange
-set-window-option -g window-status-current-fg white
-set-window-option -g window-status-current-bg default
-#set-window-option -g window-status-current-attr bright
+      # inactive window activity colors
+      setw -g window-status-activity-attr none
+      setw -g window-status-bell-attr none
+      set-window-option -g window-status-activity-fg red
+      set-window-option -g window-status-bell-fg red
+      set-window-option -g window-status-bell-bg default
+      set-window-option -g window-status-activity-bg default
 
-# pane border
-set-option -g pane-border-fg black #base02
-set-option -g pane-active-border-fg brightgreen #base01
+      # pane border
+      set-option -g pane-border-fg black
+      set-option -g pane-active-border-fg brightgreen
 
-# message text
-set-option -g message-bg black #base02
-set-option -g message-fg brightred #orange
+      # message text
+      set-option -g message-bg black
+      set-option -g message-fg brightred #orange
 
-# pane number display
-set-option -g display-panes-active-colour blue #blue
-set-option -g display-panes-colour brightred #orange
+      # pane number display
+      set-option -g display-panes-active-colour blue
+      set-option -g display-panes-colour brightred
 
-# clock
-set-window-option -g clock-mode-colour green #green
+      # clock
+      set-window-option -g clock-mode-colour green
     '';
   };
 }
