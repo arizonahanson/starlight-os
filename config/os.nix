f670208b613598507c1b020815fe01e8cf10bd72
Isaac W Hanson <isaac@starlig.ht>
2019-02-10 14:08:19 -0500

    direct update

--- config/os.nix
+++ config/os.nix
@@ -4,7 +4,11 @@
   environment.systemPackages = with pkgs; [
     (with import <nixpkgs> {}; writeShellScriptBin "os-update" ''
       #!/usr/bin/env bash
- 
+      
+      cd /tmp
+      git clone --depth 1 https://github.com/isaacwhanson/starlight-os.git starlight-os
+      cd starlight-os
+	    sudo cp -a config/. /etc/nixos/
       sudo nixos-rebuild --upgrade switch
       sudo nix-collect-garbage
       flatpak update -y
