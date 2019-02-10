0a61e208b2deb989a0c178bb3e27e4652b858655
Isaac W Hanson <isaac@starlig.ht>
2019-02-10 14:15:57 -0500

    better gitdir

--- config/os.nix
+++ config/os.nix
@@ -3,13 +3,15 @@
 {
   environment.systemPackages = with pkgs; [
     (with import <nixpkgs> {}; writeShellScriptBin "os-update" ''
-      cd /tmp
-      git clone --depth 1 https://github.com/isaacwhanson/starlight-os.git starlight-os
-      cd starlight-os
+      gitdir="$(mktemp -d --tmpdir starlight-os_XXXXXX)"
+      git clone --depth 1 https://github.com/isaacwhanson/starlight-os.git $gitdir
+      cd $gitdir
 	    sudo cp -a config/. /etc/nixos/
       sudo nixos-rebuild --upgrade switch
       sudo nix-collect-garbage
       flatpak update -y
+      cd
+      rm $gitdir -rf
     '')
     (with import <nixpkgs> {}; writeShellScriptBin "os-drop" ''
       sudo nix-collect-garbage -d
