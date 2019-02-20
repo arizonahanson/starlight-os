39e4ff47272e476208ad8262e5f762cc9b7423db
Isaac W Hanson <ihanson@sevone.com>
2019-02-20 10:27:47 -0500

    single_monocle setting changed. do not like

--- config/bspwm.nix
+++ config/bspwm.nix
@@ -49,7 +49,6 @@
       bspc config split_ratio          0.50
       bspc config borderless_monocle   true
       bspc config gapless_monocle      true
-      bspc config single_monocle       true
       bspc config focus_follows_pointer false
 
       bspc rule -a Rofi state=floating
