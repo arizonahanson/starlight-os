c4e603630c5911f959eaf2bae929b723e5f2a3c3
Isaac W Hanson <ihanson@sevone.com>
2019-02-25 11:41:32 -0500

    auto-update on startup

--- config/os.nix
+++ config/os.nix
@@ -10,20 +10,39 @@
     ./desktop.nix
     ./docker.nix
   ];
-  config.environment.systemPackages = with pkgs; [
-    (with import <nixpkgs> {}; writeShellScriptBin "os-update" ''
-      echo -e "Fetching configuration..."
-      gitdir="$(mktemp -d --tmpdir starlight-os_XXXXXX)"
-      git clone -q --depth 1 https://github.com/isaacwhanson/starlight-os.git $gitdir
-      cd $gitdir
-      make upgrade
-      cd
-      rm $gitdir -rf
-    '')
-    (with import <nixpkgs> {}; writeShellScriptBin "os-drop" ''
-      sudo nix-collect-garbage -d
-      nix-env --delete-generations old
-    '')
-  ];
+  config = let osupdate = (with import <nixpkgs> {}; writeShellScriptBin "os-update" ''
+    renice 19 -p $$
+    echo -e "Fetching configuration..."
+    gitdir="$(mktemp -d --tmpdir starlight-os_XXXXXX)"
+    git clone -q --depth 1 https://github.com/isaacwhanson/starlight-os.git $gitdir
+    cd $gitdir
+    make upgrade
+    cd /tmp
+    #rm $gitdir -rf
+  '');
+  in
+  {
+    environment.systemPackages = with pkgs; [
+      (osupdate)
+      (with import <nixpkgs> {}; writeShellScriptBin "os-drop" ''
+        sudo nix-collect-garbage -d
+        nix-env --delete-generations old
+      '')
+    ];
+    systemd.services.auto-update = {
+      serviceConfig.Type = "oneshot"; 
+      wantedBy = [ "multi-user.target" ];
+      unitConfig.X-StopOnRemoval = false;
+      environment = config.nix.envVars // {
+        inherit (config.environment.sessionVariables) NIX_PATH;
+        HOME = "/root";
+      } // config.networking.proxy.envVars;
+      path = with pkgs; [ (osupdate) gnumake gitMinimal utillinux sudo nix gnutar xz.bin config.nix.package.out config.system.build.nixos-rebuild ];
+      restartIfChanged = false;
+      script = ''
+        os-update
+      ''; 
+    }; 
+  };
 }
 
