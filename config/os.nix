64d2238db58edaa9d8d82f0de780f4a565509c9e
Isaac W Hanson <isaac@starlig.ht>
2019-04-01 17:14:47 -0400

    os-squish

--- config/os.nix
+++ config/os.nix
@@ -28,6 +28,22 @@
         sudo nix-collect-garbage -d
         nix-env --delete-generations old
       '')
+      (with import <nixpkgs>  {}; writeShellScriptBin "os-squish" ''
+      	echo -e "\n\e[1;34m\e[0m Deduplicating system..."
+	device="$(findmnt -nvo SOURCE /)"
+	mntpnt="$(mktemp -d --tmpdir duperemove-XXXXXX)"
+	mkdir -p $mntpnt
+	sudo mount -o compress=lzo $device $mntpnt
+	pushd $mntpnt >/dev/null
+	sudo duperemove -Ardh system/nix >/dev/null
+	echo -e "\n\e[1;34m\e[0m Rebalancing filesystem..."
+	sudo btrfs balance start -dusage=70 -musage=70 $mntpnt
+	popd >/dev/null
+	sudo umount $mntpnt
+	rmdir $mntpnt
+	echo -e "\n\e[1;34m\e[0m Discarding unused blocks..."
+	sudo fstrim -av
+      '')
     ];
   };
 }
