3abdf1c760ccf9dc0463d719c439d4c9677e261c
Isaac W Hanson <ihanson@sevone.com>
2019-04-01 11:20:18 -0400

    comment-out init defaults

--- init/configuration.nix
+++ init/configuration.nix
@@ -4,17 +4,16 @@
   imports = [ ./os.nix ];
 
   networking.hostName = "myhost";
-    
-  starlight.desktop = true;
+  #starlight.desktop = true;
   #starlight.docker = false;
   #starlight.server = false;
-  starlight.proaudio = {
-    enable = false;
-    device = "none";
-    capture = "hw:0";
-    playback = "hw:0";
-    rate = 44100;
-    periods = 3;
-    frames = 1024;
-  };
+  #starlight.proaudio = {
+  #  enable = false;
+  #  device = "none";
+  #  capture = "hw:0";
+  #  playback = "hw:0";
+  #  rate = 44100;
+  #  periods = 3;
+  #  frames = 1024;
+  #};
 }
