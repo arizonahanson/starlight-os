bc2039d844489ba422b8363696311c0594bbfce3
Isaac W Hanson <isaac@starlig.ht>
2019-05-17 12:03:52 -0400

    update avail options in `init/configuration.nix`

--- init/configuration.nix
+++ init/configuration.nix
@@ -4,16 +4,24 @@
   imports = [ ./os.nix ];
 
   networking.hostName = "myhost";
-  #starlight.desktop = true;
-  #starlight.docker = false;
-  #starlight.server = false;
-  #starlight.proaudio = {
-  #  enable = false;
-  #  device = "none";
-  #  capture = "hw:0";
-  #  playback = "hw:0";
-  #  rate = 44100;
-  #  periods = 3;
-  #  frames = 1024;
+  #starlight = {
+  # desktop = true;
+  # docker = false;
+  # server = false;
+  # proaudio = {
+  #   enable = false;
+  #   enable = false;
+  #   device = "none";
+  #   capture = "hw:0";
+  #   playback = "hw:0";
+  #   rate = 44100;
+  #   periods = 3;
+  #   frames = 1024;
+  # };
+  # palette = {
+  #   background = "#212121";
+  #   foreground = "#c7c7c7";
+  #   color0 = "#212121";
+  # };
   #};
 }
