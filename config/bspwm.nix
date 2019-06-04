b149bb48c4cd421159fd8d874c311259a00a0122
Isaac W Hanson <isaac@starlig.ht>
2019-06-04 19:21:43 -0400

    bspwm: border_width, focused_border_color

--- config/bspwm.nix
+++ config/bspwm.nix
@@ -44,9 +44,9 @@ with lib;
         xset s noblank
 
         # border color
-        bspc config focused_border_color "${palette.cursor}"
+        bspc config focused_border_color "${palette.foreground-alt}"
         bspc config normal_border_color "${palette.background-alt}"
-        bspc config border_width         2
+        bspc config border_width         3
         bspc config window_gap           0
         bspc config split_ratio          0.50
         bspc config borderless_monocle   true
