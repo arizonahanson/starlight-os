471e00b35ae8b19e6bc632c1bd5141778f7e2ee1
Isaac W Hanson <isaac@starlig.ht>
2019-06-11 19:43:34 -0400

    networking: sorta works

--- config/networking.nix
+++ config/networking.nix
@@ -17,7 +17,6 @@ with lib;
       hostName = (config.starlight.hostname);
       networkmanager = {
         enable = true;
-        #dns = "systemd-resolved";
         extraConfig = ''
           [connection]
           connection.mdns=2
@@ -39,41 +38,7 @@ with lib;
       openssh.enable = true;
       resolved = {
         enable = true;
-        fallbackDns = [ "8.8.8.8" ];
       };
     };
-    #environment.etc."NetworkManager/conf.d/extra.conf" = {
-    #  text = ''
-    #    [connection]
-    #    connection.mdns=1
-    #  '';
-    #};
-    #environment.systemPackages = with pkgs; [ nssmdns ];
-    #systemd = {
-    #  additionalUpstreamSystemUnits = [
-    #    "systemd-resolved.service"
-    #  ];
-    #  services.systemd-resolved = {
-    #    wantedBy = [ "multi-user.target" ];
-    #    restartTriggers = [ config.environment.etc."systemd/resolved.conf".source ];
-    #  };
-    #};
-    #environment.etc = let
-    #  net = config.networking;
-    #  resolv = config.services.resolved; in {
-    #  "resolv.conf".source = "/run/systemd/resolve/resolv.conf";
-    #  "systemd/resolved.conf".text = ''
-    #    [Resolve]
-    #    ${optionalString (net.nameservers != [])
-    #      "DNS=${concatStringsSep " " net.nameservers}"}
-    #    ${optionalString (resolv.fallbackDns != [])
-    #      "FallbackDNS=${concatStringsSep " " resolv.fallbackDns}"}
-    #    ${optionalString (resolv.domains != [])
-    #      "Domains=${concatStringsSep " " resolv.domains}"}
-    #    LLMNR=${resolv.llmnr}
-    #    DNSSEC=${resolv.dnssec}
-    #    ${resolv.extraConfig}
-    #  '';
-    #};
   };
 }
