ac77acfa14f5a59561170c4d514415a615a566dd
Isaac W Hanson <isaac@starlig.ht>
2019-06-10 23:17:39 -0400

    polybar: square indicators

--- config/polybar.nix
+++ config/polybar.nix
@@ -90,19 +90,19 @@ with lib;
           format = <label-mode><label-state>
           format-foreground = ''${colors.foreground}
 
-          label-focused = "◉ "
+          label-focused = "▣ "
           label-focused-foreground = ''${colors.foreground}
           label-focused-padding = 0
 
-          label-occupied = "◍ "
+          label-occupied = "▤ "
           label-occupied-padding = 0
           label-occupied-foreground = ''${colors.foreground-alt}
 
-          label-urgent = "◍ "
+          label-urgent = "▧ "
           label-urgent-foreground = ''${colors.info}
           label-urgent-padding = 0
 
-          label-empty = "○ "
+          label-empty = "□ "
           label-empty-foreground = ''${colors.background-alt}
           label-empty-padding = 0
 
@@ -111,7 +111,7 @@ with lib;
           label-urgent-font = 4
           label-empty-font = 4
 
-          label-dimmed-focused = "◎ "
+          label-dimmed-focused = "▩ "
           label-dimmed-focused-foreground = ''${colors.foreground-alt}
 
           label-floating = "  "
