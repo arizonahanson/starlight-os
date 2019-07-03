a50dec645071dbca4e59a0babe3132a1252b8b81
Isaac W Hanson <isaac@starlig.ht>
2019-07-03 17:29:16 -0400

    iso: refactor

--- iso/iso.nix
+++ iso/iso.nix
@@ -2,23 +2,21 @@
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
-
     # Provide an initial copy of the NixOS channel so that the user
     # doesn't need to run "nix-channel --update" first.
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
     ../config/git.nix
   ];
-  environment.systemPackages = with pkgs; [
-    gnumake
-    (with import <nixpkgs> {}; writeShellScriptBin "os-config" ''
+  environment.systemPackages = let
+    os-config = (with import <nixpkgs> {}; writeShellScriptBin "os-config" ''
       cd ~
       git clone --depth 1 'https://github.com/isaacwhanson/starlight-os.git' starlight-os
       cd starlight-os
       make configure
-    '')
-    (with import <nixpkgs> {}; writeShellScriptBin "os-install" ''
+    '');
+    os-install = (with import <nixpkgs> {}; writeShellScriptBin "os-install" ''
       cd ~/starlight-os
       make install
-    '')
-  ];
+    '');
+    in [ pkgs.gnumake (os-config) (os-install) ];
 }
