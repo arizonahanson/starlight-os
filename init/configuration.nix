45643d7706be8bf3d797047679053b40cd0a5eb4
Isaac Hanson <isaac@starlig.ht>
2019-07-09 11:21:59 -0400

    desktop: don't put colors in example config (changes too often)

--- init/configuration.nix
+++ init/configuration.nix
@@ -5,23 +5,18 @@
 
   starlight = {
     hostname = "myhost";
-  # logo = "• ";
-  # desktop = true;
-  # docker = false;
-  # server = false;
-  # proaudio = {
-  #   enable = false;
-  #   device = "none";
-  #   capture = "none";
-  #   playback = "none";
-  #   rate = 44100;
-  #   periods = 2;
-  #   frames = 1024;
-  # };
-  # palette = {
-  #   background = "#1A1A1A";
-  #   foreground = "#CCCCCC";
-  #   color0 = "#1A1A1A";
-  # };
+   # logo = "• ";
+   # desktop = true;
+   # docker = false;
+   # server = false;
+   # proaudio = {
+   #   enable = false;
+   #   device = "none";
+   #   capture = "none";
+   #   playback = "none";
+   #   rate = 44100;
+   #   periods = 2;
+   #   frames = 1024;
+   # };
   };
 }
