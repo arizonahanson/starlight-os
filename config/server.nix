c980ea781e23bbb30e904e1ea7c66b69009157e0
Isaac W Hanson <isaac@starlig.ht>
2019-07-21 15:07:35 -0400

    server: duplicate ssh service

--- config/server.nix
+++ config/server.nix
@@ -19,8 +19,6 @@ in {
   # config...
   config = mkIf cfg.server {
     programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-    # Enable the OpenSSH daemon.
-    services.openssh.enable = true;
     services.nixosManual.enable = true;
     services.nixosManual.showManual = true;
   };
