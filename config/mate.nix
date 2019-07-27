c2a6d3053e1f42f510ab636ec00162b4021ef305
Isaac W Hanson <isaac@starlig.ht>
2019-07-27 16:59:40 -0400

    mate: reduce nix expr

--- config/mate.nix
+++ config/mate.nix
@@ -7,28 +7,19 @@ with lib;
     ./dconf.nix
   ];
   config = lib.mkIf config.starlight.desktop {
-    environment.variables = {
-      XDG_CURRENT_DESKTOP = "MATE";
+    environment = {
+      variables = {
+        XDG_CURRENT_DESKTOP = "MATE";
+      };
+      systemPackages = with pkgs; [
+        gnome3.dconf-editor
+      ];
+      mate.excludePackages = with pkgs.mate; [
+        engrampa eom mate-applets mate-backgrounds mate-calc mate-indicator-applet
+        mate-netbook mate-screensaver mate-sensors-applet mate-system-monitor
+        mate-user-guide mozo pluma
+      ];
     };
-    environment.systemPackages = with pkgs; [
-      gnome3.dconf-editor
-    ];
-    environment.mate.excludePackages = with pkgs; [
-        mate.engrampa
-        mate.eom
-        mate.mate-applets
-        mate.mate-backgrounds
-        mate.mate-calc
-        mate.mate-indicator-applet
-        mate.mate-netbook
-        mate.mate-screensaver
-        mate.mate-sensors-applet
-        mate.mate-system-monitor
-        mate.mate-user-guide
-        mate.mozo
-        mate.pluma
-    ];
-
     # Enable the X11 windowing system.
     services.xserver.desktopManager = {
       mate.enable = true;
