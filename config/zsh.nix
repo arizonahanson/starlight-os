4b93842758992676bd066d24acd8d3110cddd422
Isaac W Hanson <isaac@starlig.ht>
2019-08-11 20:55:37 -0400

    zsh: dircolors reset file

--- config/zsh.nix
+++ config/zsh.nix
@@ -665,51 +665,43 @@ with lib;
     };
     environment.etc.dircolors = {
       text = ''
+        # Configuration file for dircolors, a utility to help you set the
+        # LS_COLORS environment variable used by GNU ls with the --color option.
+        # Copyright (C) 1996-2019 Free Software Foundation, Inc.
+        # Copying and distribution of this file, with or without modification,
+        # are permitted provided the copyright notice and this notice are preserved.
+        # The keywords COLOR, OPTIONS, and EIGHTBIT (honored by the
+        # slackware version of dircolors) are recognized but ignored.
+        # Below are TERM entries, which can be a glob patterns, to match
+        # against the TERM environment variable to determine if it is colorizable.
+        TERM Eterm
         TERM ansi
-        TERM color-xterm
-        TERM con132x25
-        TERM con132x30
-        TERM con132x43
-        TERM con132x60
-        TERM con80x25
-        TERM con80x28
-        TERM con80x30
-        TERM con80x43
-        TERM con80x50
-        TERM con80x60
+        TERM *color*
+        TERM con[0-9]*x[0-9]*
         TERM cons25
         TERM console
+        TERM cygwin
+        TERM dtterm
         TERM gnome
-        TERM gnome-256color
+        TERM hurd
+        TERM jfbterm
+        TERM konsole
+        TERM kterm
         TERM linux
         TERM linux-c
-        TERM rxvt
-        TERM rxvt-256color
-        TERM rxvt-cygwin
-        TERM rxvt-cygwin-native
-        TERM rxvt-unicode
-        TERM rxvt-unicode-256color
-        TERM rxvt-unicode256
-        TERM screen
-        TERM screen-256color
-        TERM screen-256color-bce
-        TERM screen-bce
-        TERM screen-w
-        TERM screen.Eterm
-        TERM screen.rxvt
-        TERM screen.linux
-        TERM screen.xterm
+        TERM mlterm
+        TERM putty
+        TERM rxvt*
+        TERM screen*
         TERM st
-        TERM st-256color
         TERM terminator
+        TERM tmux*
         TERM vt100
-        TERM xterm
-        TERM xterm-16color
-        TERM xterm-256color
-        TERM xterm-color
-        TERM xterm-termite
-        # Below are the color init strings for the basic file types. A color init
-        # string consists of one or more of the following numeric codes:
+        TERM xterm*
+        # Below are the color init strings for the basic file types.
+        # One can use codes for 256 or more colors supported by modern terminals.
+        # The default color codes use the capabilities of an 8 color terminal
+        # with some additional attributes as per the following codes:
         # Attribute codes:
         # 00=none 01=bold 04=underscore 05=blink 07=reverse 08=concealed
         # Text color codes:
@@ -721,11 +713,11 @@ with lib;
         RESET 0 # reset to "normal" color
         DIR ${toANSI theme.path} # directory
         LINK target # symbolic link. (If you set this to 'target' instead of a
-        # numerical value, the color is as for the file pointed to.)
+         # numerical value, the color is as for the file pointed to.)
         MULTIHARDLINK 00 # regular file with more than one link
-        SOCK ${toANSI theme.socket} # socket
+        FIFO ${toANSI theme.socket} # pipe
+        SOCK ${toANSI theme.socket};4 # socket
         DOOR ${toANSI theme.socket};7 # door
-        FIFO ${toANSI theme.socket};4 # pipe
         BLK ${toANSI theme.string} # block device driver
         CHR ${toANSI theme.character} # character device driver
         ORPHAN ${toANSI theme.error} # symlink to nonexistent file, or non-stat'able file ...
@@ -736,207 +728,141 @@ with lib;
         STICKY_OTHER_WRITABLE ${toANSI theme.path};4;7 # dir that is sticky and other-writable (+t,o+w)
         OTHER_WRITABLE ${toANSI theme.path};7 # dir that is other-writable (o+w) and not sticky
         STICKY ${toANSI theme.path};4 # dir with the sticky bit set (+t) and not other-writable
+        # This is for files with execute permission:
         EXEC ${toANSI theme.executable}
-        .cmd ${toANSI theme.alias}
-        .exe ${toANSI theme.alias}
-        .com ${toANSI theme.alias}
-        .btm ${toANSI theme.alias}
-        .bat ${toANSI theme.alias}
+        # List any file extensions like '.gz' or '.tar' that you would like ls
+        # to colorize below. Put the extension, a space, and the color init string.
+        # (and any comments you want to add after a '#')
+        # If you use DOS-style suffixes, you may want to uncomment the following:
+        #.cmd ${toANSI theme.alias} # executables
+        #.exe ${toANSI theme.alias}
+        #.com ${toANSI theme.alias}
+        #.btm ${toANSI theme.alias}
+        #.bat ${toANSI theme.alias}
+        # Or if you want to colorize scripts even if they do not have the
+        # executable bit actually set.
         .sh ${toANSI theme.alias}
         .csh ${toANSI theme.alias}
-        .tcsh ${toANSI theme.alias}
-
-        # archives or compressed
-        .tar ${toANSI theme.warning}
-        .tgz ${toANSI theme.warning}
-        .arc ${toANSI theme.warning}
-        .arj ${toANSI theme.warning}
-        .taz ${toANSI theme.warning}
-        .lha ${toANSI theme.warning}
-        .lz4 ${toANSI theme.warning}
-        .lzh ${toANSI theme.warning}
-        .lzma ${toANSI theme.warning}
-        .tlz ${toANSI theme.warning}
-        .txz ${toANSI theme.warning}
-        .tzo ${toANSI theme.warning}
-        .t7z ${toANSI theme.warning}
-        .zip ${toANSI theme.warning}
-        .z ${toANSI theme.warning}
-        .Z ${toANSI theme.warning}
-        .dz ${toANSI theme.warning}
-        .gz ${toANSI theme.warning}
-        .lrz ${toANSI theme.warning}
-        .lz ${toANSI theme.warning}
-        .lzo ${toANSI theme.warning}
-        .xz ${toANSI theme.warning}
-        .bz2 ${toANSI theme.warning}
-        .bz ${toANSI theme.warning}
-        .tbz ${toANSI theme.warning}
-        .tbz2 ${toANSI theme.warning}
-        .tz ${toANSI theme.warning}
-        .deb ${toANSI theme.warning}
-        .rpm ${toANSI theme.warning}
-        .jar ${toANSI theme.warning}
-        .war ${toANSI theme.warning}
-        .ear ${toANSI theme.warning}
-        .sar ${toANSI theme.warning}
-        .rar ${toANSI theme.warning}
-        .alz ${toANSI theme.warning}
-        .ace ${toANSI theme.warning}
-        .zoo ${toANSI theme.warning}
-        .cpio ${toANSI theme.warning}
-        .7z ${toANSI theme.warning}
-        .rz ${toANSI theme.warning}
-        .cab ${toANSI theme.warning}
+         # archives or compressed
+        .tar 01;31
+        .tgz 01;31
+        .arc 01;31
+        .arj 01;31
+        .taz 01;31
+        .lha 01;31
+        .lz4 01;31
+        .lzh 01;31
+        .lzma 01;31
+        .tlz 01;31
+        .txz 01;31
+        .tzo 01;31
+        .t7z 01;31
+        .zip 01;31
+        .z 01;31
+        .dz 01;31
+        .gz 01;31
+        .lrz 01;31
+        .lz 01;31
+        .lzo 01;31
+        .xz 01;31
+        .zst 01;31
+        .tzst 01;31
+        .bz2 01;31
+        .bz 01;31
+        .tbz 01;31
+        .tbz2 01;31
+        .tz 01;31
+        .deb 01;31
+        .rpm 01;31
+        .jar 01;31
+        .war 01;31
+        .ear 01;31
+        .sar 01;31
+        .rar 01;31
+        .alz 01;31
+        .ace 01;31
+        .zoo 01;31
+        .cpio 01;31
+        .7z 01;31
+        .rz 01;31
+        .cab 01;31
+        .wim 01;31
+        .swm 01;31
+        .dwm 01;31
+        .esd 01;31
         # image formats
-        .jpg ${toANSI theme.pattern}
-        .jpeg ${toANSI theme.pattern}
-        .gif ${toANSI theme.pattern}
-        .bmp ${toANSI theme.pattern}
-        .pbm ${toANSI theme.pattern}
-        .pgm ${toANSI theme.pattern}
-        .ppm ${toANSI theme.pattern}
-        .tga ${toANSI theme.pattern}
-        .xbm ${toANSI theme.pattern}
-        .xpm ${toANSI theme.pattern}
-        .tif ${toANSI theme.pattern}
-        .tiff ${toANSI theme.pattern}
-        .png ${toANSI theme.pattern}
-        .svg ${toANSI theme.pattern}
-        .svgz ${toANSI theme.pattern}
-        .mng ${toANSI theme.pattern}
-        .pcx ${toANSI theme.pattern}
-        .mov ${toANSI theme.pattern}
-        .mpg ${toANSI theme.pattern}
-        .mpeg ${toANSI theme.pattern}
-        .m2v ${toANSI theme.pattern}
-        .mkv ${toANSI theme.pattern}
-        .webm ${toANSI theme.pattern}
-        .ogm ${toANSI theme.pattern}
-        .mp4 ${toANSI theme.pattern}
-        .m4v ${toANSI theme.pattern}
-        .mp4v ${toANSI theme.pattern}
-        .vob ${toANSI theme.pattern}
-        .qt ${toANSI theme.pattern}
-        .nuv ${toANSI theme.pattern}
-        .wmv ${toANSI theme.pattern}
-        .asf ${toANSI theme.pattern}
-        .rm ${toANSI theme.pattern}
-        .rmvb ${toANSI theme.pattern}
-        .flc ${toANSI theme.pattern}
-        .avi ${toANSI theme.pattern}
-        .fli ${toANSI theme.pattern}
-        .flv ${toANSI theme.pattern}
-        .gl ${toANSI theme.pattern}
-        .dl ${toANSI theme.pattern}
-        .xcf ${toANSI theme.pattern}
-        .xwd ${toANSI theme.pattern}
-        .yuv ${toANSI theme.pattern}
-        .cgm ${toANSI theme.pattern}
-        .emf ${toANSI theme.pattern}
-        # http://wiki.xiph.org/index.php/MIME_Types_and_File_Extensions
-        .ogv ${toANSI theme.pattern}
-        .ogx ${toANSI theme.pattern}
-        # documents
-        .pdf   ${toANSI theme.string}
-        .doc   ${toANSI theme.string}
-        .docx  ${toANSI theme.string}
-        .xps   ${toANSI theme.string}
-        .xpsx  ${toANSI theme.string}
-        .odg   ${toANSI theme.string}
-        .odt   ${toANSI theme.string}
-        .odf   ${toANSI theme.string}
-        .xls   ${toANSI theme.string}
-        .xlsx  ${toANSI theme.string}
-        .dia   ${toANSI theme.string}
-        .rtf   ${toANSI theme.string}
-        .dot   ${toANSI theme.string}
-        .dotx  ${toANSI theme.string}
-        .ppt   ${toANSI theme.string}
-        .pptx  ${toANSI theme.string}
-        .fla   ${toANSI theme.string}
-        .psd   ${toANSI theme.string}
+        .jpg 01;35
+        .jpeg 01;35
+        .mjpg 01;35
+        .mjpeg 01;35
+        .gif 01;35
+        .bmp 01;35
+        .pbm 01;35
+        .pgm 01;35
+        .ppm 01;35
+        .tga 01;35
+        .xbm 01;35
+        .xpm 01;35
+        .tif 01;35
+        .tiff 01;35
+        .png 01;35
+        .svg 01;35
+        .svgz 01;35
+        .mng 01;35
+        .pcx 01;35
+        .mov 01;35
+        .mpg 01;35
+        .mpeg 01;35
+        .m2v 01;35
+        .mkv 01;35
+        .webm 01;35
+        .ogm 01;35
+        .mp4 01;35
+        .m4v 01;35
+        .mp4v 01;35
+        .vob 01;35
+        .qt 01;35
+        .nuv 01;35
+        .wmv 01;35
+        .asf 01;35
+        .rm 01;35
+        .rmvb 01;35
+        .flc 01;35
+        .avi 01;35
+        .fli 01;35
+        .flv 01;35
+        .gl 01;35
+        .dl 01;35
+        .xcf 01;35
+        .xwd 01;35
+        .yuv 01;35
+        .cgm 01;35
+        .emf 01;35
+        # https://wiki.xiph.org/MIME_Types_and_File_Extensions
+        .ogv 01;35
+        .ogx 01;35
         # audio formats
-        .aac ${toANSI theme.number}
-        .au ${toANSI theme.number}
-        .flac ${toANSI theme.number}
-        .m4a ${toANSI theme.number}
-        .mid ${toANSI theme.number}
-        .midi ${toANSI theme.number}
-        .mka ${toANSI theme.number}
-        .mp3 ${toANSI theme.number}
-        .mpc ${toANSI theme.number}
-        .ogg ${toANSI theme.number}
-        .ra ${toANSI theme.number}
-        .wav ${toANSI theme.number}
-        # http://wiki.xiph.org/index.php/MIME_Types_and_File_Extensions
-        .oga ${toANSI theme.number}
-        .opus ${toANSI theme.number}
-        .spx ${toANSI theme.number}
-        .xspf ${toANSI theme.number}
-
-        # encrypted/key formats
-        .gpg  ${toANSI theme.number}
-        .pgp  ${toANSI theme.number}
-        .pub  ${toANSI theme.number}
-        .crt  ${toANSI theme.number}
-        .pem  ${toANSI theme.number}
-        .asc  ${toANSI theme.number}
-        .3des ${toANSI theme.number}
-        .aes  ${toANSI theme.number}
-        .enc  ${toANSI theme.number}
-        .sig  ${toANSI theme.number}
-        *key  ${toANSI theme.number}
-
-
-        # source code files
-        .c    ${toANSI theme.alias}
-        .h    ${toANSI theme.alias}
-        .java ${toANSI theme.alias}
-        .js   ${toANSI theme.alias}
-        .jsx  ${toANSI theme.alias}
-        .vim  ${toANSI theme.alias}
-        .py   ${toANSI theme.alias}
-        .go   ${toANSI theme.alias}
-        .nix  ${toANSI theme.alias}
-
-        # data files
-        .json         ${toANSI theme.string}
-        .xml          ${toANSI theme.string}
-        .iml          ${toANSI theme.string}
-        .properties   ${toANSI theme.string}
-        .yml          ${toANSI theme.string}
-        .yaml         ${toANSI theme.string}
-        .toml         ${toANSI theme.string}
-
-        # txt files
-        .txt     ${toANSI theme.foreground-alt}
-        .TXT     ${toANSI theme.foreground-alt}
-        .log     ${toANSI theme.foreground-alt}
-
-        # readme, etc
-        .md        ${toANSI theme.foreground-alt}
-        *README    ${toANSI theme.foreground-alt}
-        *README.TXT ${toANSI theme.foreground-alt}
-        *README.txt ${toANSI theme.foreground-alt}
-
-        # html
-        .html   ${toANSI theme.pattern}
-        .htm    ${toANSI theme.pattern}
-        .css    ${toANSI theme.pattern}
-        .less   ${toANSI theme.pattern}
-
-        # other
-        *~            ${toANSI theme.background-alt}
-        *.pid         ${toANSI theme.background-alt}
-        *desktop.ini  ${toANSI theme.background-alt}
-        *Desktop.ini  ${toANSI theme.background-alt}
-        .ICEauthority ${toANSI theme.background-alt}
-        .Xauthority   ${toANSI theme.background-alt}
-        .xsession-errors ${toANSI theme.background-alt}
-        .old          ${toANSI theme.background-alt}
-        .hidden       ${toANSI theme.background-alt}
-        .zcompdump    ${toANSI theme.background-alt}
+        .aac 00;36
+        .au 00;36
+        .flac 00;36
+        .m4a 00;36
+        .mid 00;36
+        .midi 00;36
+        .mka 00;36
+        .mp3 00;36
+        .mpc 00;36
+        .ogg 00;36
+        .ra 00;36
+        .wav 00;36
+        # https://wiki.xiph.org/MIME_Types_and_File_Extensions
+        .oga 00;36
+        .opus 00;36
+        .spx 00;36
+        .xspf 00;36
+        # additional highlighting
+        .pid ${toANSI theme.background-alt}
+        *~ ${toANSI theme.background-alt}
+        .gitignore ${toANSI theme.foreground-alt}
       '';
     };
   };
