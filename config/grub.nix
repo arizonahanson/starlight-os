acc18fb588bf4cc05dbe87ebf36b2bf9c312f8d3
Isaac W Hanson <isaac@starlig.ht>
2019-08-30 22:16:23 -0400

    efi support

--- config/grub.nix
+++ config/grub.nix
@@ -1,11 +1,29 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+
+with lib;
 
 {
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-    version = 2;
-    useOSProber = true;
+  options.starlight = {
+    efi = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        If enabled, will boot ufi mode
+      '';
+    };
+  };
+  config.boot.loader = if config.starlight.efi then {
+    timeout = 10;
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  } else {
+    timeout = 10;
+    grub = {
+      enable = true;
+      device = "/dev/sda";
+      version = 2;
+      useOSProber = true;
+    };
   };
 }
 
