9d477f628df704360cb8aff750b1a345821d9cf8
Isaac W Hanson <isaac@starlig.ht>
2019-10-22 20:36:36 -0400

    go stable

--- config/base.nix
+++ config/base.nix
@@ -88,6 +88,6 @@ with lib;
     # compatible, in order to avoid breaking some software such as database
     # servers. You should change this only after NixOS release notes say you
     # should.
-    system.stateVersion = "unstable"; # Did you read the comment?
+    system.stateVersion = "19.09"; # Did you read the comment?
   }];
 }
