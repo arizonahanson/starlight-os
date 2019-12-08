0a2b4d713d3b1445b37a2717fe3e322a685866ce
Isaac W Hanson <isaac@starlig.ht>
2019-12-08 14:45:45 -0500

    vim: borrow Julia's latex-to-unicode functionality 😄

--- config/vim.nix
+++ config/vim.nix
@@ -33,6 +33,7 @@
               let g:vimcache='/tmp/.vim-'.$USER.'/'
               let g:gutentags_cache_dir=g:vimcache.'ctags//'
               let g:gutentags_exclude_filetypes=["gitcommit", "gitrebase"]
+              let g:latex_to_unicode_file_types = ".*"
               let g:mucomplete#completion_delay=500
               let g:mucomplete#enable_auto_at_startup=1
               let g:mucomplete#no_mappings=1
