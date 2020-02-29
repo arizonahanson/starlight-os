3c2e5e0193511ea3969733ba6b6c438b8316dba7
Isaac W Hanson <isaac@starlig.ht>
2020-02-29 09:55:08 -0500

    upgrade timer

--- config/base.nix
+++ config/base.nix
@@ -68,10 +68,31 @@ with lib;
           enable = true;
           fileSystems = [ "/" ];
         };
-        nix.gc = {
-          automatic = true;
-          dates = "*-*-* 00/2:00:00";
-          options = "--delete-older-than 14d";
+				systemd.timers.nixos-upgrade = {
+					description = "NixOS Upgrade Timer";
+					wantedBy = [ "timers.target" ];
+					timerConfig.OnStartupSec = "10min";
+				};
+        systemd.services.nixos-upgrade = {
+					description = "NixOS Upgrade";
+					restartIfChanged = false;
+					unitConfig.X-StopOnRemoval = false;
+					serviceConfig.Type = "oneshot";
+
+					environment = config.nix.envVars //
+						{ inherit (config.environment.sessionVariables) NIX_PATH;
+							HOME = "/root";
+						} // config.networking.proxy.envVars;
+
+					path = with pkgs; [ coreutils gnutar xz.bin gzip gitMinimal config.nix.package.out ];
+
+					script = let
+							nixos-rebuild = "${config.system.build.nixos-rebuild}/bin/nixos-rebuild";
+						in
+						''
+              ${nixos-rebuild} switch --upgrade
+              nix-collect-garbage --delete-older-than 4w
+						'';
         };
         nix.autoOptimiseStore = true;
         systemd.tmpfiles.rules = [
