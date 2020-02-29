67c87b87af5dd1b2750b938023541c3f78ecaa20
Isaac W Hanson <isaac@starlig.ht>
2020-02-29 12:27:17 -0500

    split auto-upgrade nix

--- config/base.nix
+++ config/base.nix
@@ -9,6 +9,7 @@ with lib;
     ./git.nix
     ./tmux.nix
     ./vim.nix
+    ./autoupgrade.nix
   ];
   options.starlight = {
     localTime = mkOption {
@@ -64,43 +65,6 @@ with lib;
           "/".options = [ "compress-force=zstd" ];
           "/home".options = [ "compress-force=zstd" ];
         };
-        services.btrfs.autoScrub = {
-          enable = true;
-          fileSystems = [ "/" ];
-        };
-				systemd.timers.nixos-upgrade = {
-					description = "NixOS Upgrade Timer";
-					wantedBy = [ "timers.target" ];
-					timerConfig.OnStartupSec = "5min";
-				};
-        systemd.services.nixos-upgrade = {
-					description = "NixOS Upgrade Service";
-					restartIfChanged = false;
-					unitConfig.X-StopOnRemoval = false;
-					serviceConfig.Type = "oneshot";
-
-					environment = config.nix.envVars //
-						{ inherit (config.environment.sessionVariables) NIX_PATH;
-							HOME = "/root";
-						} // config.networking.proxy.envVars;
-
-					path = with pkgs; [ coreutils gnutar xz.bin gzip gitMinimal config.nix.package.out ];
-
-					script = let
-            nixos-gc = "${config.nix.package.out}/bin/nix-collect-garbage";
-            nixos-rebuild = "${config.system.build.nixos-rebuild}/bin/nixos-rebuild";
-						in
-						''
-              ${nixos-gc} --delete-older-than 2w
-              ${nixos-rebuild} switch --upgrade
-						'';
-        };
-        nix.autoOptimiseStore = true;
-        systemd.tmpfiles.rules = [
-          "d /run/cache/ 1771 - users"
-          "d /var/config/ 1771 - users 2w"
-          "e /var/tmp/ - - - 2w"
-        ];
         nixpkgs.config.allowUnfree = true;
         environment.systemPackages = with pkgs; [
           gnumake
