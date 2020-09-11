efd5fc37afc71e8a268b12255ba5f9162415cd5a
Isaac W Hanson <ihanson@sevone.com>
2020-09-11 12:39:50 -0400

    better ale support

--- config/vim.nix
+++ config/vim.nix
@@ -12,11 +12,20 @@
             beforePlugins = ''
               let g:ale_cache_executable_check_failures=1
               let g:ale_completion_enabled=1
-              let g:ale_lint_delay=500
+              let g:ale_close_preview_on_insert=1
+              let g:ale_cursor_detail=1
+              let g:ale_fixers={
+                \ 'typescript': ['eslint'],
+                \ 'javascript': ['eslint'],
+                \ 'nix': ['nixpkgs-fmt']
+              \ }
+              let g:ale_fix_on_save=1
+              let g:ale_hover_to_preview=1
+              let g:ale_lint_delay=400
+              "let g:ale_linters_ignore={'javascript':['tsc','tsserver','tslint']}
               let g:ale_sign_error=' '
               let g:ale_sign_warning=' '
               let g:ale_sign_info=' '
-              let g:ale_linters_ignore={'javascript':['tsc','tsserver','tslint']}
               let g:foldchar='╍'
               let g:gitgutter_override_sign_column_highlight=0
               let g:gitgutter_map_keys=0
@@ -197,6 +206,7 @@
               let &t_EI="\e[${toString cfg.commandCursor} q"
               "--- using the mouse
               set mouse=a
+              "set ttymouse=xterm
               "--- messages and info
               set shortmess+=I
               set shortmess+=c
@@ -290,8 +300,10 @@
               nnoremap <silent> ]r :ALENextWrap<CR>
               nnoremap <silent> <F3> :set number!<CR>
               nnoremap <silent> <F4> :set foldenable!<CR>
-              nnoremap <silent> <F5> :ALEToggle<CR>
-              nnoremap <silent> <F6> :GitGutterToggle<CR>
+              nnoremap <silent> <F5> :GitGutterToggle<CR>
+              nnoremap <silent> <F6> :ALEToggle<CR>
+              nnoremap <silent> <F7> :ALEHover<CR>
+              nnoremap <silent> <F8> :ALEDetail<CR>
             '';
             plug.plugins = let
               vim-gdscript3 = pkgs.vimUtils.buildVimPlugin {
