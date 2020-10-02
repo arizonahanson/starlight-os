01b0bf9060d5312a3afc80777b6a5022e2093a98
Isaac W Hanson <ihanson@sevone.com>
2020-10-02 15:54:08 -0400

    translucent terminal

--- config/desktop.nix
+++ config/desktop.nix
@@ -57,7 +57,7 @@ with lib;
       type = types.float;
       default = 0.75;
       description = ''
-        compton shadow opacity
+        picom shadow opacity
         (default 0.75)
       '';
     };
@@ -65,7 +65,7 @@ with lib;
       type = types.int;
       default = 16;
       description = ''
-        compton shadow radius
+        picom shadow radius
         (default 16)
       '';
     };
@@ -303,7 +303,7 @@ with lib;
           reload-desktop = (
             with import <nixpkgs> {}; writeShellScriptBin "reload-desktop" ''
               ${procps}/bin/pkill -USR1 -x sxhkd
-              ${procps}/bin/pkill -TERM -x compton
+              ${procps}/bin/pkill -TERM -x picom
               ${procps}/bin/pkill -TERM -x polybar
               ${bspwm}/bin/bspc wm -r
               say 'Reloaded desktop' 'Desktop components have been reloaded'
@@ -360,15 +360,18 @@ with lib;
       '';
     };
     services = {
-      compton = let cfg = config.starlight; in
+      picom = let cfg = config.starlight; in
         {
           enable = true;
+          backend = "glx";
+          activeOpacity = 1.0;
+          inactiveOpacity = 1.0;
+          opacityRules = [
+            "95:class_g = 'terminal'"
+          ];
           shadow = true;
           shadowOffsets = [ (cfg.shadowRadius * -1) (cfg.shadowRadius / -2) ];
           shadowOpacity = cfg.shadowOpacity;
-          settings = {
-            shadow-radius = cfg.shadowRadius;
-          };
           shadowExclude = [
             "name = 'Polybar tray window'"
             "_GTK_FRAME_EXTENTS@:c"
@@ -376,6 +379,54 @@ with lib;
           fade = true;
           fadeDelta = 3;
           fadeSteps = [ 0.03125 0.03125 ];
+          settings = {
+            shadow-radius = cfg.shadowRadius;
+            frame-opacity = 0.9;
+            mark-wmwin-focused = true;
+            mark-ovredir-focused = true;
+            detect-rounded-corners = true;
+            detect-client-opacity = true;
+            unredir-if-possible = false;
+            detect-transient = true;
+            detect-client-leader = true;
+            blur = {
+              method = "dual_kawase";
+              strength = 8;
+              background = false;
+              background-frame = false;
+              background-fixed = false;
+            };
+            blur-background = true;
+            blur-background-frame = true;
+            blur-kern = "3x3box";
+            blur-background-fixed = true;
+						round-borders = 2;
+						round-borders-rule = [
+							"2:window_type = 'unknown'"
+              "2:window_type = 'toolbar'"
+              "2:window_type = 'utility'"
+							"2:window_type = 'splash'"
+							"2:window_type = 'dialog'"
+							"2:window_type = 'normal'"
+							"2:window_type = 'notification'"
+						];
+						round-borders-exclude = [
+							"class_g = 'mpv'"
+						];
+						corner-radius = 15;
+						rounded-corners-exclude = [
+							"window_type = 'desktop'"
+							"window_type = 'toolbar'"
+							"window_type = 'menu'"
+							"window_type = 'dropdown_menu'"
+							#"window_type = 'popup_menu'",
+							#"window_type = 'notification'",
+							"window_type = 'dnd'"
+							"window_type = 'dock'"
+						  #"class_g = 'Spotify'",
+							"class_g = 'mpv'"
+						];
+          };
         };
       flatpak = {
         enable = true;
