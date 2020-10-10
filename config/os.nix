bd1efbfa7d461c9fd13b56785b6fb7b42fd6e104
Isaac W Hanson <isaac@starlig.ht>
2020-10-09 23:14:40 -0400

    add nixos-rebuild to os-upgrade path

--- config/os.nix
+++ config/os.nix
@@ -10,43 +10,44 @@
     ./desktop.nix
     ./docker.nix
   ];
-  config = let
-    theme = config.starlight.theme;
-    toANSI = num: if num <= 7 then "00;3${toString num}" else "01;3${toString (num - 8)}";
-    os-cmd = (
-      with import <nixpkgs> {}; writeShellScriptBin "os" ''
-        cmd="$1"
-        renice 19 -p $$ >/dev/null
-        echo -e "Fetching configuration..."
-        gitdir="$(mktemp -d -p "$XDG_CACHE_HOME" os-XXXX)"
-        git clone -q --depth 1 https://github.com/isaacwhanson/starlight-os.git "$gitdir" || exit 1
-        pushd $gitdir >/dev/null || exit 1
-        make $1
-        popd >/dev/null
-        rm "$gitdir" -rf
-      ''
-    );
-    squish = (
-      with import <nixpkgs> {}; writeShellScriptBin "squish" ''
-        device="$(findmnt -nvo SOURCE /)"
-        mntpnt="$(mktemp -d -p "$XDG_CONFIG_HOME" squish-XXXX)"
-        mkdir -p $mntpnt || exit 1
-        sudo mount -o compress-force=zstd,noatime $device $mntpnt || exit 1
-        pushd $mntpnt >/dev/null || exit 1
-        echo -e "\n\e[${toANSI theme.path}m\e[0m Compressing system..."
-        sudo btrfs filesystem defragment -r -v -czstd $mntpnt/*
-        sync
-        echo -e "\n\e[${toANSI theme.path}m\e[0m Deduplicating system..."
-        sudo duperemove -Ardhv --hash=xxhash $(ls -d */nix) | grep "net change"
-        sync
-        popd >/dev/null
-        sudo umount $mntpnt || exit 1
-        rmdir $mntpnt || exit 1
-        echo -e "\n\e[${toANSI theme.path}m\e[0m Discarding unused blocks..."
-        sudo fstrim -av
-      ''
-    );
-  in
+  config =
+    let
+      theme = config.starlight.theme;
+      toANSI = num: if num <= 7 then "00;3${toString num}" else "01;3${toString (num - 8)}";
+      os-cmd = (
+        with import <nixpkgs> { }; writeShellScriptBin "os" ''
+          cmd="$1"
+          renice 19 -p $$ >/dev/null
+          echo -e "Fetching configuration..."
+          gitdir="$(mktemp -d -p "$XDG_CACHE_HOME" os-XXXX)"
+          git clone -q --depth 1 https://github.com/isaacwhanson/starlight-os.git "$gitdir" || exit 1
+          pushd $gitdir >/dev/null || exit 1
+          make $1
+          popd >/dev/null
+          rm "$gitdir" -rf
+        ''
+      );
+      squish = (
+        with import <nixpkgs> { }; writeShellScriptBin "squish" ''
+          device="$(findmnt -nvo SOURCE /)"
+          mntpnt="$(mktemp -d -p "$XDG_CONFIG_HOME" squish-XXXX)"
+          mkdir -p $mntpnt || exit 1
+          sudo mount -o compress-force=zstd,noatime $device $mntpnt || exit 1
+          pushd $mntpnt >/dev/null || exit 1
+          echo -e "\n\e[${toANSI theme.path}m\e[0m Compressing system..."
+          sudo btrfs filesystem defragment -r -v -czstd $mntpnt/*
+          sync
+          echo -e "\n\e[${toANSI theme.path}m\e[0m Deduplicating system..."
+          sudo duperemove -Ardhv --hash=xxhash $(ls -d */nix) | grep "net change"
+          sync
+          popd >/dev/null
+          sudo umount $mntpnt || exit 1
+          rmdir $mntpnt || exit 1
+          echo -e "\n\e[${toANSI theme.path}m\e[0m Discarding unused blocks..."
+          sudo fstrim -av
+        ''
+      );
+    in
     {
       systemd = {
         timers.os-upgrade = {
@@ -64,6 +65,7 @@
           };
           path = with pkgs; [
             config.nix.package.out
+            config.system.build.nixos-rebuild
             bash
             coreutils
             gitMinimal
@@ -75,7 +77,8 @@
             xz.bin
           ];
           environment = config.nix.envVars //
-            { inherit (config.environment.sessionVariables) NIX_PATH;
+            {
+              inherit (config.environment.sessionVariables) NIX_PATH;
               HOME = "/root";
             } // config.networking.proxy.envVars;
           script = ''
