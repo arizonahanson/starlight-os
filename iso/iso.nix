3c135fe05ced0ff029922401a9c8050602da40dc
Isaac W Hanson <isaac@starlig.ht>
2020-10-11 17:44:23 -0400

    nixpkgs-fmt iso/iso.nix

--- iso/iso.nix
+++ iso/iso.nix
@@ -8,21 +8,22 @@
     ../config/colors.nix
     ../config/git.nix
   ];
-  environment.systemPackages = let
-    os-config = (
-      with import <nixpkgs> {}; writeShellScriptBin "os-config" ''
-        git clone -q --depth 1 https://github.com/isaacwhanson/starlight-os.git ~/starlight-os || exit 1
-        cd ~/starlight-os || exit 1
-        bash scripts/partition
-        bash scripts/config
-        echo "Edit the file '/mnt/etc/nixos/configuration.nix' then run 'os-install'."
-      ''
-    );
-    os-install = (
-      with import <nixpkgs> {}; writeShellScriptBin "os-install" ''
-        nixos-install --no-root-passwd --root /mnt
-      ''
-    );
-  in
+  environment.systemPackages =
+    let
+      os-config = (
+        with import <nixpkgs> { }; writeShellScriptBin "os-config" ''
+          git clone -q --depth 1 https://github.com/isaacwhanson/starlight-os.git ~/starlight-os || exit 1
+          cd ~/starlight-os || exit 1
+          bash scripts/partition
+          bash scripts/config
+          echo "Edit the file '/mnt/etc/nixos/configuration.nix' then run 'os-install'."
+        ''
+      );
+      os-install = (
+        with import <nixpkgs> { }; writeShellScriptBin "os-install" ''
+          nixos-install --no-root-passwd --root /mnt
+        ''
+      );
+    in
     [ pkgs.gnumake (os-config) (os-install) ];
 }
