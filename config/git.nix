7e0f216af0bd14e3a3ee891ec6b804db9f51bb4a
Isaac W Hanson <isaac@starlig.ht>
2021-02-18 19:48:34 -0500

    keep git for now

--- config/git.nix
+++ config/git.nix
@@ -5,6 +5,18 @@
     let
       theme = config.starlight.theme;
       toANSI = num: if num <= 7 then "00;3${toString num}" else "01;3${toString (num - 8)}";
+      git-minimal = (
+        (pkgs.git.overrideAttrs (oldAttrs: rec {
+          doInstallCheck = false;
+          NIX_CFLAGS_COMPILE = "-march=native";
+        })).override {
+          guiSupport = false;
+          pythonSupport = false;
+          perlSupport = false;
+          withManual = false; # time consuming
+          withLibsecret = true;
+        }
+      );
       git-config = ''
         [core]
           autocrlf = false
@@ -91,7 +103,7 @@
           for repo in $(find -L . -maxdepth 7 -iname '.git' -type d -printf '%P\0' 2>/dev/null | xargs -0 dirname | sort); do
             echo -e "\e[${toANSI theme.executable}m  \e[${toANSI theme.path}m$repo \e[0m(\e[${toANSI theme.function}m$@\e[0m)"
             pushd $repo >/dev/null
-            git "$@"
+            ${git-minimal}/bin/git "$@"
             popd >/dev/null
             echo
           done
@@ -99,6 +111,7 @@
       );
     in
     {
+      systemPackages = [ (git-minimal) (git-all) pkgs.tig ];
       etc.gitconfig =
         if config.services.xserver.enable then
           {
