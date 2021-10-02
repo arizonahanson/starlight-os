0c2e06e79e825ef29070f6ffc1e6d2c459544e3f
Isaac W Hanson <isaac@starlig.ht>
2021-10-02 15:23:15 -0400

    cleanup

--- config/audio.nix
+++ config/audio.nix
@@ -6,17 +6,6 @@ with lib;
   config = mkMerge [
     (
       mkIf config.starlight.desktop {
-        # Enable sound.
-        #sound.enable = true;
-        #users.users.starlight.extraGroups = [ "audio" ];
-        #hardware.pulseaudio = {
-        #  enable = true;
-        #  package = pkgs.pulseaudioFull;
-        #  daemon.config = {
-        #    default-sample-format = "s24-32le";
-        #    default-sample-rate = "44100";
-        #  };
-        #};
         environment.systemPackages = with pkgs; [
           playerctl
           sound-theme-freedesktop
@@ -39,64 +28,7 @@ with lib;
           LV2_PATH = "/nix/var/nix/profiles/default/lib/lv2:/var/run/current-system/sw/lib/lv2:~/.lv2";
           DSSI_PATH = "/nix/var/nix/profiles/default/lib/dssi:/var/run/current-system/sw/lib/dssi:~/.dssi";
         };
-        #boot = {
-        #  kernelModules = [ "snd-seq" "snd-rawmidi" ];
-        #  postBootCommands = ''
-        #    echo 2048 > /sys/class/rtc/rtc0/max_user_freq
-        #    echo 2048 > /proc/sys/dev/hpet/max-user-freq
-        #    setpci -v -d *:* latency_timer=b0
-        #    setpci -v -s ''$(lspci | grep -i audio | awk '{print ''$1}') latency_timer=ff
-        #  '';
-        #};
-        #powerManagement.cpuFreqGovernor = "performance";
-        #security.pam.loginLimits = [
-        #  { domain = "@audio"; item = "memlock"; type = "-"; value = "unlimited"; }
-        #  { domain = "@audio"; item = "rtprio"; type = "-"; value = "99"; }
-        #  { domain = "@audio"; item = "nofile"; type = "soft"; value = "99999"; }
-        #  { domain = "@audio"; item = "nofile"; type = "hard"; value = "99999"; }
-        #];
-        #services = {
-        #  udev = {
-        #    extraRules = ''
-        #      KERNEL=="rtc0", GROUP="audio"
-        #      KERNEL=="hpet", GROUP="audio"
-        #    '';
-        #  };
-        #  cron.enable = false;
-        #};
-        #systemd.user.services.pulseaudio.environment.DISPLAY = ":0";
-        /*systemd.user.services.autojack =
-          let
-          cfg = config.starlight.proaudio;
-          in
-          {
-          serviceConfig.Type = "simple";
-          wantedBy = [ "default.target" ];
-          path = [ pkgs.jack2 pkgs.a2jmidid pkgs.pulseaudioFull ];
-          environment = {
-          DISPLAY = ":0";
-          };
-          after = [ "pulseaudio.service" ];
-          enable = true;
-          script = ''
-          sleep 5
-          jack_control ds alsa
-          jack_control dps device '${cfg.device}'
-          jack_control dps capture '${cfg.capture}'
-          jack_control dps playback '${cfg.playback}'
-          jack_control dps rate ${toString cfg.rate}
-          jack_control dps nperiods ${toString cfg.periods}
-          jack_control dps period ${toString cfg.frames}
-          jack_control start
-          a2j_control ehw
-          a2j_control start
-          sleep infinity
-          '';
-          preStop = ''
-          a2j_control exit
-          jack_control exit
-          '';
-          };*/
+        powerManagement.cpuFreqGovernor = "performance";
         /*programs.zsh.shellAliases = with pkgs; {
           fluidsynth = "${fluidsynth}/bin/fluidsynth ${soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2";
           };*/
