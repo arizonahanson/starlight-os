8e0793ce8a2427ad614c72e4da380106a9aca63d
Arizona Hanson <arizona@starlig.ht>
2021-10-03 14:20:08 -0400

    extend upgrade timer to near a day

--- config/os.nix
+++ config/os.nix
@@ -56,7 +56,7 @@
           description = "StarlightOS Upgrade Timer";
           wantedBy = [ "timers.target" ];
           timerConfig = {
-            OnStartupSec = "1h";
+            OnStartupSec = "22h";
             OnCalendar = "*-*-* 04:00:00";
           };
         };
@@ -91,7 +91,7 @@
             booted="$(readlink /run/booted-system/{initrd,kernel,kernel-modules})"
             latest="$(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
             if [ ! "$booted" = "$latest" ]; then
-              shutdown -r +30
+              shutdown -r +10
             else
               ${os-cmd}/bin/os upgrade && ${os-cmd}/bin/os expire
             fi
